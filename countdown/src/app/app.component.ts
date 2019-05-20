9f1767f9069e1af008417ab90fcd8a6fde0fd732
Erik Wahlström <erik.wahlstrom@sandvik.com>
2019-05-20 14:05:22 +0200

    A few minor changes

--- countdown/src/app/app.component.ts
+++ countdown/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   workingDaysLeft: number;
   workingDaysLeftArray: WorkingDay[];
   today: Date;
+  timeLeftDate: Date;
 
   constructor() {
     this.source = timer(0, 1000);
@@ -22,14 +23,15 @@ export class AppComponent {
     const subscribe = this.source.subscribe(val => {
       const now = new Date();
       this.today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-      this.timeLeft = (this.endDate.getTime() / msToDays) - (this.today.getTime() / msToDays);
+      this.timeLeft = (this.endDate.getTime() / msToDays) - (now.getTime() / msToDays);
+      this.timeLeftDate = new Date((this.endDate.getTime()) - (now.getTime()));
       const nonworkingDays = [
         new Date(2019, 1, 18),
         new Date(2019, 4, 18),
         new Date(2019, 4, 19),
         new Date(2019, 4, 25),
         new Date(2019, 4, 26),
-        new Date(2019, 4, 31),
+        new Date(2019, 4, 30),
         new Date(2019, 5, 1),
         new Date(2019, 5, 2),
         new Date(2019, 5, 6),
@@ -37,7 +39,7 @@ export class AppComponent {
         new Date(2019, 5, 9),
       ].filter(x => x > this.today);
       this.workingDaysLeft = this.timeLeft - (nonworkingDays.length);
-      const wholeDaysLeft = Math.round(this.timeLeft);
+      const wholeDaysLeft = Math.trunc(this.timeLeft);
       this.workingDaysLeftArray = [];
       for (let i = 1; i <= wholeDaysLeft; i++) {
         const nextDay = new Date(new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + i));
